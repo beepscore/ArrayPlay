adc6951dd3a4ca58965a3095cbe423d166dbbf8f
beepscore <support@beepscore.com>
2012-02-12 18:35:33 -0800

    Add comments and references.

--- ArrayPlayTests/ArrayModelTests.m
+++ ArrayPlayTests/ArrayModelTests.m
@@ -26,17 +26,24 @@
 
 
 -(void)testAddObjectToArrayThenChangeObject {
+    // http://stackoverflow.com/questions/5686767/changing-array-elements-after-shallow-copying-the-array
     
     ArrayModel *testArrayModel = [[ArrayModel alloc] init];
     
-    NSNumber *testNumber = [NSNumber numberWithInt:2];
+    NSNumber *testNumber = [NSNumber numberWithInt:65];
+    // numbers 0-12 are singletons??
+    // http://stackoverflow.com/questions/4270121/why-nsnumber-points-to-the-same-address-when-value-are-equals
+    NSLog(@"&testNumber = %llu", &testNumber);
     testArrayModel.myArray = [NSArray arrayWithObject:testNumber];
     
-    // change testNumber, expect it doesn't array element
-    testNumber = [NSNumber numberWithInt:3];
-    NSNumber *expectedNumber = [NSNumber numberWithInt:2];
+    // change testNumber to point to a new object at a different address
+    testNumber = [NSNumber numberWithInt:66];
+    NSLog(@"&testNumber = %llu", &testNumber);
+    // [testArrayModel.myArray objectAtIndex:0] still points to the original NSNumber
+    NSNumber *expectedNumber = [NSNumber numberWithInt:65];
     NSNumber *actualNumber = [testArrayModel.myArray objectAtIndex:0];
-    
+
+    // uses isEqualTo: to test objects
     STAssertEqualObjects(expectedNumber, actualNumber, nil);
 }
 
