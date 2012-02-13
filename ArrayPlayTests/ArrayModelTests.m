9597d71100420aa3715b12ae12878f93099481fa
beepscore <support@beepscore.com>
2012-02-12 22:16:01 -0800

    In testAddObjectToArrayThenChangeObject set property myArray to correct type NSMutableArray.
    Add testInitSetsMyArrayEmpty.

--- ArrayPlayTests/ArrayModelTests.m
+++ ArrayPlayTests/ArrayModelTests.m
@@ -33,7 +33,7 @@
     NSNumber *testNumber = [NSNumber numberWithInteger:65];
     // aside: NSNumbers 0-12 are singletons??
     // http://stackoverflow.com/questions/4270121/why-nsnumber-points-to-the-same-address-when-value-are-equals
-
+    
     NSLog(@"testNumber value : %i", [testNumber integerValue]);
     // compiler warns "Conversion specifies type 'unsigned long long' but the argument has type 'NSNumber'"
     NSLog(@"testNumber address : %llu", testNumber);
@@ -44,20 +44,33 @@
     STAssertEquals(65, [testNumber integerValue], nil);
     
     // [testArrayModel.myArray objectAtIndex:0] to value of pointer variable testNumber
-    testArrayModel.myArray = [NSArray arrayWithObject:testNumber];
+    [testArrayModel.myArray addObject:testNumber];
     
     // assign testNumber to point to another object at a different address
     testNumber = [NSNumber numberWithInteger:66];
     NSLog(@"testNumber value : %i", [testNumber integerValue]);
     // compiler warns "Conversion specifies type 'unsigned long long' but the argument has type 'NSNumber'"
     NSLog(@"testNumber address : %llu", testNumber);
-
+    
     // [testArrayModel.myArray objectAtIndex:0] still points to the original NSNumber
     NSNumber *expectedNumber = [NSNumber numberWithInteger:65];
     NSNumber *actualNumber = [testArrayModel.myArray objectAtIndex:0];
-
+    
     // uses isEqualTo: to test objects
     STAssertEqualObjects(expectedNumber, actualNumber, nil);
+    
+    [testArrayModel.myArray addObject:@"boo"];
+    [testArrayModel.myArray replaceObjectAtIndex:1 withObject:@"far"] ;
+    STAssertEqualObjects(@"far", [testArrayModel.myArray objectAtIndex:1], nil);
+}
+
+
+-(void)testInitSetsMyArrayEmpty {
+    
+    ArrayModel *testArrayModel = [[ArrayModel alloc] init];
+    NSMutableArray *expectedArray = [NSMutableArray array];
+    
+    STAssertEqualObjects(expectedArray, testArrayModel.myArray, nil);    
 }
 
 @end
